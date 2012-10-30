5263577ea03e9e102f6d7f21ba06967212e13165
joshbyrom <joshbyrom@gmail.com>
2012-10-30 12:56:44 -0400

    added growth and fire logic

--- src/js/Logics.js
+++ src/js/Logics.js
@@ -11,11 +11,24 @@ var Logic = function(cellspace) {
 	//    probabilities   //
 	//--------------------//
 	this.growth_prob = 0.05;
+  this.burn_prob = 0.4; // when neighbors are on fire
+  this.burned_up_prob = 1.0;  
 	this.lightning_prob = 0.0003;
 	
 	this.get_growth_prob_for_cell = function(cell) {
 		return (1 + cell.get_neighbors_in_state(this.neighbor_range, 'tree-filled-land').length) * this.growth_prob;
-	}
+	};
+  
+  this.get_fire_prob_for_cell = function(cell) {
+    if(cell.get_state() !== 'tree-filled-land') return 0;
+   
+    var burning_neighbors_len = 
+      cell.get_neighbors_in_state(this.neighbor_range, 'on-fire').length;
+    if(burning_neighbors_len === 0) return this.lightning_prob;
+    else {
+      return this.burn_prob;
+    }
+  };
 	
 	// update
 	this.update = function() {
@@ -34,14 +47,22 @@ var Logic = function(cellspace) {
 	this.handle_cell = function(cell) {
 		var state = cell.get_state();
 		
-		if(state === 'unknown') {
-			var growth = this.get_growth_prob_for_cell(cell);
-			var chance = Math.random();
-			if(chance < growth) {
-				cell.state = 'tree-filled-land';
-			} else {
-				cell.state = 'empty-land';
-			}
-		}
+		if(state === 'unknown' || state === 'empty-land') {
+			var growth_chance = this.get_growth_prob_for_cell(cell);
+      this.roll(cell, growth_chance, 'empty-land', 'tree-filled-land');
+    } else if(state === 'tree-filled-land') {
+      var burn_chance = this.get_fire_prob_for_cell(cell);
+      this.roll(cell, burn_chance, 'tree-filled-land', 'on-fire');
+    } else if(state === 'on-fire') {
+      this.roll(cell, this.burned_up_prob, 'on-fire', 'empty-land');
+    }
 	};
+  
+  this.roll = function(cell, threshold, above, below) {
+    if(Math.random() >= threshold) {
+      cell.state = above;
+    } else {
+      cell.state = below;
+    }
+  };
 }
